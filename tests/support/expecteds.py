20dff92f25fda2bfd5299815b75552e5e668e959
Aung Khant Maw <aungkhantmaw64@gmail.com>
2023-01-02 19:25:43 +0700

    Add expected raw DataFrame

--- tests/support/expecteds.py
+++ tests/support/expecteds.py
@@ -1,4 +1,5 @@
 import os
+import pandas
 
 EXPECTED_SAMPLE_PATH = "..\\data\\raw_samples\\Acetone"
 EXPECTED_SAMPLE_NAME = "Acetone"
@@ -10,10 +11,30 @@ EXPECTED_SAMPLE_LIST = ["Acetone",
                         "Toluene"]
 
 EXPECTED_IMAGES_PATH = os.path.join(EXPECTED_SAMPLE_PATH, "images")
-
+EXPECTED_CSV_PATH = os.path.join(EXPECTED_SAMPLE_PATH, "serialdata.csv")
 CORRECT_SAMPLE_STRUCTURE = ["images",
                             "serialdata.csv"]
 
 VALID_IMAGE_NAMES = ["1.jpg",
                      "2.jpg",
                      "3.jpg"]
+
+EXPECTED_RAW_DATA = pandas.DataFrame(data={'MQ-2': [3.5, 3.5],
+                                           'MQ-3': [3.5, 3.5],
+                                           'MQ-5': [3.5, 3.5],
+                                           'MQ-6': [3.5, 3.5],
+                                           'MQ-8': [3.5, 3.5],
+                                           'MQ-9': [3.5, 3.5],
+                                           'MQ-135': [3.5, 3.5],
+                                           'MQ-138': [3.5, 3.5]},
+                                     index=[0, 1])
+
+EXPECTED_VOLTAGE_TRANSIENT = pandas.DataFrame(data={"MQ-2": [1.5, 1.5],
+                                                    "MQ-3": [1.5, 1.5],
+                                                    "MQ-5": [1.5, 1.5],
+                                                    "MQ-6": [1.5, 1.5],
+                                                    "MQ-8": [1.5, 1.5],
+                                                    "MQ-9": [1.5, 1.5],
+                                                    "MQ-135": [1.5, 1.5],
+                                                    "MQ-138": [1.5, 1.5]},
+                                              index=[0, 1])
