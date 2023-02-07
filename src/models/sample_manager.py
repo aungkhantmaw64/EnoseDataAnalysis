1469dba5ebf79fe8bb605eb13c79d5e8f6205273
Aung Khant Maw <aungkhantmaw64@gmail.com>
2023-02-07 11:25:37 +0700

    added implementation for check_csv method

--- src/models/sample_manager.py
+++ src/models/sample_manager.py
@@ -1,5 +1,11 @@
+import os
+
 
 class SamplePathManager:
 
     def __init__(self) -> None:
         pass
+
+    def check_csv(self, path: str) -> bool:
+        items_under_path = os.listdir(path)
+        return "serialdata.csv" in items_under_path
--- tests/test_SamplePathManager.py
+++ tests/test_SamplePathManager.py
@@ -1,5 +1,23 @@
 from models.sample_manager import SamplePathManager
+from unittest.mock import MagicMock
+import os
 
 
-def test_checks_if_dir_has_valid_structure():
-    pass
+def test_SampleManager_ChecksIfSampleFolderHasCsvFile():
+    """
+    The path under test doesn't exist.
+    We will mock os module to get fake return values
+    GasName
+        |_images
+        |   |_1.jpg
+        |   |_2.jpg
+        |   |_etc.
+        |_serialdata.csv
+
+    """
+    os.listdir = MagicMock(return_value=["images",
+                                         "serialdata.csv"])
+    path_under_test = "GasName"
+    path_manager = SamplePathManager()
+    assert path_manager.check_csv(path_under_test)
+    os.listdir.assert_called_with(path_under_test)
