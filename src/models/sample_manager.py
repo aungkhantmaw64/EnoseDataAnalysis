2f39ec787be0c6f7fd2f6123b5d96826fef4e709
Aung Khant Maw <aungkhantmaw64@gmail.com>
2023-02-07 11:27:49 +0700

    added docstring for check_csv

--- src/models/sample_manager.py
+++ src/models/sample_manager.py
@@ -7,5 +7,13 @@ class SamplePathManager:
         pass
 
     def check_csv(self, path: str) -> bool:
+        """Checks if serialdata.csv file exist in the path
+
+        Args:
+            path (str): sample folder/path
+
+        Returns:
+            bool: true if it exists, false otherwise
+        """
         items_under_path = os.listdir(path)
         return "serialdata.csv" in items_under_path
